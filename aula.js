adc2f3d24587ecaf6a14ad43bafd3c34881d7956
Rony Mesquita da Silva <ronymesquitadasilva@gmail.com>
2022-02-25 21:35:34 -0300

    Retomar da seção 1.5 da apostila 02 falando sobre vetores

---
+++ aula.js
@@ -0,0 +1,27 @@
+// == ===
+// console.log([] == false)
+// console.log(null == undefined)
+// console.log(null == null)
+// console.log(1 == 1)
+// console.log(1 === '1')
+
+// var idade = 18
+// console.log('Oi, ' + nome)
+// if (idade >= 18) {
+//     var nome = 'João'
+//     console.log('Parabéns, ' + nome + '. Você pode dirigir.')
+//     console.log(`Parabéns, ${nome}. Você pode dirigir.`)
+// }
+// console.log('Até mais, ' + nome)
+
+//começando...
+// const nome = 'José'
+// const idade = 27
+// const a = 1
+
+// let c = 2
+// let b = 'abc'
+// let endereco = `Rua K, 12`
+
+// var d = 2 + 3
+// var d = 'abcd'
